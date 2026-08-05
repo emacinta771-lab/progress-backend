b2fd968f56a63586d9cf3c8a24d82c39f95a6e55
emacinta771-lab <emacinta771@gmail.com>
2026-08-05 17:19:28 +0200

    updated backend

--- server.js
+++ server.js
@@ -97,6 +97,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
   'https://progress-xi-three.vercel.app',
+  'https://phunzira-tau.vercel.app',
   'https://sms-school.vercel.app',
   ...(process.env.FRONTEND_URL ? [process.env.FRONTEND_URL] : [])
 ];
